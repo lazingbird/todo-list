f0cf2471bb112a34c336f8ffcb9dc6a52a9f8523
Lucas <lazingbird@protonmail.com>
2022-12-26 15:34:31 -0300

    update basic structure

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -16,8 +16,63 @@ function renderUI() {
   //   create and append two different sections to divide the main container
   const projectsDisplay = createHtmlElement('div', 'projectsDisplay');
   const tasksDisplay = createHtmlElement('div', 'tasksDisplay');
+
   mainContainer.appendChild(projectsDisplay);
   mainContainer.appendChild(tasksDisplay);
+
+  // create projectsDisplay's navigation menu
+  const navItems = ['Inbox'];
+
+  const navContainer = createHtmlElement('nav', 'navContainer');
+  const navList = createHtmlElement('ul', 'navList');
+  navContainer.appendChild(navList);
+
+  navItems.forEach((item) => {
+    let newItem = createHtmlElement('li', null, ['navItem']);
+    let newItemIcon = createHtmlElement(
+      'span',
+      null,
+      ['material-symbols-outlined'],
+      'all_inbox'
+    );
+    let newItemButton = createHtmlElement('button', null, null, item);
+
+    newItem.appendChild(newItemIcon);
+    newItem.appendChild(newItemButton);
+    navList.appendChild(newItem);
+  });
+
+  projectsDisplay.appendChild(navContainer);
+
+  // create and append tasksDisplay's basic interface and create an "add new task" button
+
+  const tasksDisplayInterface = createHtmlElement(
+    'div',
+    'tasksDisplayInterface'
+  );
+
+  const tasksDisplayTitle = createHtmlElement(
+    'h2',
+    'tasksDisplayTitle',
+    null,
+    'Inbox'
+  );
+  const addNewTaskButton = createHtmlElement('button', 'addNewTaskButton');
+  const addNewTaskIcon = createHtmlElement(
+    'span',
+    null,
+    ['material-symbols-outlined'],
+    'add'
+  );
+  const addNewTaskText = createHtmlElement('p', null, null, 'Add new task');
+
+  addNewTaskButton.appendChild(addNewTaskIcon);
+  addNewTaskButton.appendChild(addNewTaskText);
+
+  tasksDisplayInterface.appendChild(tasksDisplayTitle);
+  tasksDisplayInterface.appendChild(addNewTaskButton);
+
+  tasksDisplay.appendChild(tasksDisplayInterface);
 }
 
 export { renderUI };
---
+++ src/modules/tasks-handler.js
@@ -0,0 +1,7 @@
+import { createHtmlElement, contentElement } from '..';
+
+defaultProject = [];
+
+const taskFactory = (name, date) => {
+  return { name, date };
+};
